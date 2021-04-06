e206e289d5f4c18b066e3222ebe00b5955fb7b61
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2021-04-06 15:45:07 +0300

    adding comments

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// one to many relationship (user has many complaints & the complaint belongs into one user)
 class CreateUsersTable extends Migration
 {
     /**
@@ -14,6 +15,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            // the primary key 
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
