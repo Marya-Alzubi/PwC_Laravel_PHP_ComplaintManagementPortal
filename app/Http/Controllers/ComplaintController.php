e79c29811787ce1e101d0e31c91dec8560990695
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2021-04-06 15:35:08 +0300

    adding comments

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -16,15 +16,18 @@ class ComplaintController extends Controller
      *
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
      */
+    // user and admin auth middlewares
     public function __construct(){
         $this->middleware('auth')->only('index','create', 'store' );
         $this->middleware('auth:admin')->only('readAllComplaints','edit', 'update' , 'delete');
     }
+    // fetch all complaints to read in admin dashboard
     public function readAllComplaints()
     {
         $complaints = Complaint::orderByDesc('id')->get();
         return view("admin.read_all_complaints", compact('complaints'));
     }
+    // show the complaint form to user with a status_store session which works with sweet alert 
     public function index()
     {
         return view("client.create_complaint")->with('status_store', 'Your complaint request has been sent successfully ');
@@ -34,6 +37,7 @@ class ComplaintController extends Controller
      *
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
      */
+    // the redirection after storing a new record into data base with a status_store session which works with sweet alert 
     public function create()
     {
 //        $complaints = Complaint::all();
@@ -46,8 +50,10 @@ class ComplaintController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
      */
+    // store a new record into database  & sending an email to the user with his data 
     public function store(Request $request)
     {
+        //request validation
         $request->validate([
             'generation' => 'Required',
             'serial_number' => 'Required|min:4',
@@ -56,13 +62,16 @@ class ComplaintController extends Controller
             'device_type' => 'Required',
             'problem_cause' => 'Required',
         ]);
+        // for multi select list 
         $input = $request->all();
         $input[ 'errors' ] = json_encode( $input[ 'errors' ] );
+        // set the is_urgent field value
         if($request->is_urgent == null){
             $is_urgent = 'off';
              } else {
             $is_urgent = 'on';
              }
+        // storing intodatabase
         Complaint::create([
             "user_id" =>auth()->id() ,
             "device_type" =>$request->device_type ,
@@ -73,6 +82,7 @@ class ComplaintController extends Controller
             "problem_cause" =>$request->problem_cause ,
             "is_urgent" => $is_urgent ,
         ]);
+        // sending an email using Gmail (the configration in .env file)
         $user = User::where('id', Auth::user()->id)->first();
         $to_name = $user->name ;
         $to_email = $user->email ;
@@ -86,26 +96,14 @@ class ComplaintController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Complaint  $complaint
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
-     */
-    public function show(Complaint $complaint)
-    {
-
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Complaint  $complaint
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
      */
+    // fetch the spacified complaint from database into a spacified page
     public function edit(Complaint $complaint)
     {
-//        $complaint = Complaint::find($complaint);
-//        dd($complaint);
         return view('admin.update_complaint' , compact('complaint'));
     }
 
@@ -116,12 +114,14 @@ class ComplaintController extends Controller
      * @param  \App\Complaint  $complaint
      * @return \Illuminate\Http\RedirectResponse
      */
+    // update the specified complaint in the data base the send an email to the user with his new updates
     public function update(Request $request,  $id)
     {
-//        $complaint->update($request->all());
+        // updating the record
         db::table('complaints')->where('id', $id)->update([
             'status' => $request->status
         ]);
+        // sending an email using Gmail (the configration in .env file)
         $user = User::where('id', Auth::user()->id)->first();
         $to_name = $user->name ;
         $to_email = $user->email ;
@@ -131,18 +131,7 @@ class ComplaintController extends Controller
                 ->subject('PwC ');
             $message->from('marya.testing@gmail.com','PwC Updated Status Mail');
         });
+        // return to the spacified redirect with a status_store session which works with sweet alert 
         return redirect(route('admin.all.complaints'))->with('status_store', 'tht updated status has been sent to user by email');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Complaint  $complaint
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Complaint $complaint)
-    {
-        $complaint->delete();
-        return redirect(route('admin.all.complaints'));
     }
 }
